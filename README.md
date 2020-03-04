412b12387f897fb17994ea09d03b419b442a9891
mithra-1998 <mitworkmail@gmail.com>
2020-03-04 17:23:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-90033-task-prompt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-90033-task-prompt)
---
+++ index.js
@@ -0,0 +1,17 @@
+let person= prompt("Please enter your name", "Mithra");
+
+let a = prompt("Start Slice",)
+
+let b = prompt("End Slice",)
+
+if(isNaN(a) == 0 || isNaN(b) == 0){
+  alert("Please enter numbers only")
+  let a = prompt("Start Slice",)
+
+let b = prompt("End Slice",)
+alert(person.slice(parseInt(a),parseInt(b)))
+}
+else{
+
+alert(person.slice(parseInt(a),parseInt(b)))
+}
